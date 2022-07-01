149819b4f63d994fd7400a153a77c305a67a7014
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-01 14:03:43 +0400

    TypeScipt Credentials

---
+++ config.ts
@@ -0,0 +1,22 @@
+export default (env: string) => {
+    const config = {
+        test: {
+            host: 'http://localhost',
+            port: 3000,
+            db_url: 'mongodb://localhost:27017'
+        },
+        development: {
+            host: 'http://localhost',
+            port: 3000,
+            db_url: 'mongodb://localhost:27017'
+        },
+        production: {
+            host: 'http://localhost',
+            port: 3000,
+            db_url: 'mongodb://localhost:27017'
+        }
+    }
+
+    // return Object.keys(config).includes(env) ? config[env] : config['development']
+    return env === 'test' || env === 'development' || env === 'production' ? config[env] : config['development']
+}
